ae8cf1a82ca7d69048d1fcb12668348a47651e08
henggelermeier-create <henggelermeier@gmail.com>
2026-05-04 17:35:40 +0200

    Import product gallery styles

--- webapp/src/main.jsx
+++ webapp/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
+import './product-gallery.css'
 import Root from './Root.jsx'
 
 createRoot(document.getElementById('root')).render(
